acaa1a9cdef0a483e6d2bd3be5cb81ed96d1d4bb
trangttq1105 <trangttq1105@gmail.com>
2026-02-28 19:17:36 +0700

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -76,7 +76,7 @@ document.getElementById("generateBtn").onclick = () => {
     const fullText =
 `${sentence} ${customText}
 
-#CatForCashEP7`;
+#CatForCash`;
 
     const replyUrl =
 `https://twitter.com/intent/tweet?in_reply_to=${tweetId}&text=${encodeURIComponent(fullText)}`;
